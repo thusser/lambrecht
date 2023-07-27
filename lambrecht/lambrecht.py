bd7eeb8d527c5ad6f12ecb01cb315713fcd295c3
Tim-Oliver Husser <thusser@uni-goettingen.de>
2023-07-27 22:17:15 +0200

    might work now

--- lambrecht/lambrecht.py
+++ lambrecht/lambrecht.py
@@ -1,21 +1,21 @@
-import logging
-import time
-import serial
+import datetime
 import threading
-
-from .report import Report, SensorsReport
 
 
 class Lambrecht:
     """Class that operates a Lambrecht meteo weather station."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, dev_file: str, *args, **kwargs):
         """
 
         Args:
+            dev_file: Device file for input.
             *args:
             **kwargs:
         """
+
+        # store
+        self._dev_file = dev_file
 
         # poll thread
         self._closing = None
@@ -56,24 +56,31 @@ class Lambrecht:
         and the type of response.  The other methods normally only access the most current report.
         """
 
-        # init
-        serial_errors = 0
-        sleep_time = self._thread_sleep
-        last_report = None
-        raw_data = b''
+        # init values
+        values = {"temp": 0.0, "windspeed": 0.0, "winddir": 0.0, "humid": 0.0, "dewpoint": 0.0, "press": 0.0}
 
-        # loop until closing
-        while not self._closing.is_set():
-            # actually read next line and process it
-            raw_data += self._conn.read()
+        # open file
+        with open(self._dev_file, "r") as dev:
+            # loop until closing
+            while not self._closing.is_set():
+                # read next line, and skip lines not starting with $
+                line = dev.readline().strip()
+                if not line.startswith("$"):
+                    continue
 
-            # extract messages
-            msgs, raw_data = self._extract_messages(raw_data)
+                # split line and check first token
+                s = line.split()
+                if s[0] == "$WIMTA":
+                    values["temp"] = float(s[1])
+                elif s[0] == "$WIMWV":
+                    values["winddir"] = float(s[1])
+                    values["windspeed"] = float(s[3])
+                elif s[0] == "$WIMHU":
+                    values["humid"] = float(s[1])
+                    values["dewpoint"] = float(s[3])
+                elif s[0] == "$WIMMB":
+                    values["press"] = float(s[1])
+                    self._callback(datetime.datetime.utcnow(), values)
 
-            # analyse it
-            for msg in msgs:
-                self._analyse_message(msg)
-                last_report = time.time()
 
-
-__all__ = ['Lambrecht']
+__all__ = ["Lambrecht"]
--- lambrecht/web.py
+++ lambrecht/web.py
@@ -2,12 +2,14 @@ import argparse
 import datetime
 import json
 import os
+from typing import Optional
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 import logging
+import numpy as np
 
 from lambrecht.influx import Influx
 from .lambrecht import Lambrecht
@@ -16,13 +18,12 @@ from .lambrecht import Lambrecht
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         app: Application = self.application
-        self.render(os.path.join(os.path.dirname(__file__), 'template.html'),
-                    current=app.current, history=app.history)
+        self.render(os.path.join(os.path.dirname(__file__), "template.html"), current=app.current, history=app.history)
 
 
 class JsonHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
-        self.set_header('Content-Type', 'application/json')
+        self.set_header("Content-Type", "application/json")
 
     def get(self, which):
         """JSON output of data.
@@ -35,54 +36,50 @@ class JsonHandler(tornado.web.RequestHandler):
         """
 
         # get record
-        if which == 'current':
-            record = self.application.current
-        elif which == 'average':
-            record = self.application.average
+        if which == "current":
+            time, values = self.application.current
+        elif which == "average":
+            time, values = self.application.average
         else:
             raise tornado.web.HTTPError(404)
 
         # get data
-        data = {'time': record.time.strftime('%Y-%m-%d %H:%M:%S')}
-        for c in ['ambientTemperature', 'relativeHumidityPercentage', 'windSpeed',
-                  'skyMinusAmbientTemperature', 'rainSensor']:
-            data[c] = record.data[c] if c in record.data else 'N/A'
+        values["time"] = time
 
         # send to client
-        self.write(json.dumps(data))
+        self.write(json.dumps(values))
 
 
 class Application(tornado.web.Application):
     def __init__(self, log_file: str = None, *args, **kwargs):
         # static path
-        static_path = os.path.join(os.path.dirname(__file__), 'static_html/')
+        static_path = os.path.join(os.path.dirname(__file__), "static_html/")
 
         # init tornado
-        tornado.web.Application.__init__(self, [
-            (r'/', MainHandler),
-            (r'/(.*).json', JsonHandler),
-            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': static_path})
-        ])
+        tornado.web.Application.__init__(
+            self,
+            [
+                (r"/", MainHandler),
+                (r"/(.*).json", JsonHandler),
+                (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": static_path}),
+            ],
+        )
 
         # init other stuff
-        self.current = None
-        self.reports = []
-        self.history = []
+        self.current: dict[str, float] = {}
+        self.buffer = list(dict[str, float])
+        self.history = list(dict[str, float])
         self.log_file = log_file
-        self.thresholds = None
-        self.wetness = None
-        self.wetness_calib = None
-        self.thermo_calib = None
 
         # load history
         self._load_history()
 
     @property
-    def average(self):
-        return self.history[0] if len(self.history) > 0 else Report()
+    def average(self) -> tuple[Optional[datetime.datetime, dict[str, float]]]:
+        return self.history[0] if len(self.history) > 0 else (None, {})
 
-    def callback(self, report):
-        pass
+    def callback(self, time: datetime.datetime, values: dict[str, float]):
+        self.current = (time, values)
 
     def _load_history(self):
         """Load history from log file"""
@@ -92,47 +89,47 @@ class Application(tornado.web.Application):
             return
 
         # open file
-        with open(self.log_file, 'r') as csv:
+        with open(self.log_file, "r") as csv:
             # check header
-            if csv.readline() != 'time,T_ambient,humidity,windspeed,dT_sky,raining\n':
-                logging.error('Invalid log file format.')
+            if csv.readline() != "time,temp,windspeed,winddir,humid,dewpoint,press\n":
+                logging.error("Invalid log file format.")
                 return
 
             # read lines
             for line in csv:
                 # split and check
-                s = line.split(',')
-                if len(s) != 6:
-                    logging.error('Invalid log file format.')
+                split = line.split(",")
+                if len(split) != 7:
+                    logging.error("Invalid log file format.")
                     continue
 
-                # create report and fill it
-                report = AverageSensorsReport([])
-                report.time = datetime.datetime.strptime(s[0], '%Y-%m-%dT%H:%M:%S')
-                report.data = {
-                    'ambientTemperature': float(s[1]),
-                    'relativeHumidityPercentage': float(s[2]),
-                    'windSpeed': float(s[3]),
-                    'skyMinusAmbientTemperature': float(s[4]),
-                    'rainSensor': s[5] == 'True',
-                }
-                self.history.append(report)
+                # read line
+                time = datetime.datetime.strptime(split[0], "%Y-%m-%dT%H:%M:%S")
+                values = [float(s) for s in split[1:]]
+                self.buffer.append((time, values))
 
         # crop
         self._crop_history()
 
     def _crop_history(self):
         # sort history
-        self.history = sorted(self.history, key=lambda h: h.time, reverse=True)
+        self.history = sorted(self.history, key=lambda h: h[0], reverse=True)
 
         # crop to 10 entries
         if len(self.history) > 10:
             self.history = self.history[:10]
 
     def sched_callback(self):
+        # check
+        if len(self.buffer) == 0:
+            return
+
         # average reports
-        average = AverageSensorsReport(self.reports)
-        self.history.append(average)
+        time = self.buffer[0][0]
+        average = {k: np.mean([b[1][k] for b in self.buffer]) for k in self.current.keys()}
+
+        # add to history
+        self.history.append((time, average))
         self._crop_history()
 
         # write to log file?
@@ -140,26 +137,30 @@ class Application(tornado.web.Application):
             # does it exist?
             if not os.path.exists(self.log_file):
                 # write header
-                with open(self.log_file, 'w') as csv:
-                    csv.write('time,T_ambient,humidity,windspeed,dT_sky,raining\n')
+                with open(self.log_file, "w") as csv:
+                    csv.write("time,temp,windspeed,winddir,humid,dewpoint,press\n")
 
             # write line
-            with open(self.log_file, 'a') as csv:
-                fmt = '{time},' \
-                      '{ambientTemperature:.2f},' \
-                      '{relativeHumidityPercentage:.2f},' \
-                      '{windSpeed:.2f},' \
-                      '{skyMinusAmbientTemperature:.2f},' \
-                      '{rainSensor}\n'
-                csv.write(fmt.format(time=average.time.strftime('%Y-%m-%dT%H:%M:%S'), **average.data))
+            with open(self.log_file, "a") as csv:
+                fmt = (
+                    "{time},"
+                    "{temp.2f},"
+                    "{winddirspeed:.2f},"
+                    "{winddir:.2f},"
+                    "{humid:.2f},"
+                    "{dewpoint:.2f},"
+                    "{press:.2f}\n"
+                )
+                csv.write(fmt.format(time=time.strftime("%Y-%m-%dT%H:%M:%S"), **average))
 
         # reset reports
-        self.reports = []
+        self.buffer.clear()
 
 
 def main():
     # parser
     parser = argparse.ArgumentParser("Lambrecht meteo data logger")
+    parser.add_argument("--dev-file", type=str, default="/dev/ttyS0", help="Device filename")
     parser.add_argument("--log-file", type=str, help="Log file for average values")
     parser.add_argument("--influx", type=str, help="Four strings containing URL, token, org, and bucket", nargs=4)
     args = parser.parse_args()
@@ -175,10 +176,10 @@ def main():
     influx.start()
 
     # callback method
-    def callback(report: Report):
+    def callback(time: datetime.datetime, values: dict[str, float]):
         # forward to application and influx
-        application.callback(report)
-        influx(report)
+        application.callback(time, values)
+        influx(time, values)
 
     # start polling
     lambrecht.start_polling(callback)
@@ -205,5 +206,5 @@ def main():
     sched.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
