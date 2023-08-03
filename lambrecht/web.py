bae9664e28930a4f98349ad72ae9ccae2c0f2a89
Tim-Oliver Husser <thusser@uni-goettingen.de>
2023-08-03 14:18:44 +0200

    fixed bug

--- lambrecht/web.py
+++ lambrecht/web.py
@@ -21,7 +21,7 @@ COLS = COLUMNS.split(",")[1:]
 
 def datetime_from_utc_to_local(utc_datetime):
     now_timestamp = time.time()
-    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
     return utc_datetime + offset
 
 
